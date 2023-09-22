777e22fc2b0938b34f5857c9794433c8abc069c4
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-22 11:41:11 +0300

    Edit mandatory task 0: indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 
 file { '/tmp/school':
-    ensure => 'present',
+    ensure  => 'present',
     content => 'I love Puppet',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
