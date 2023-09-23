e68cdee0bc6701b07b58728d1f5809c638972cce
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-23 20:32:21 +0300

    Add advanced task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Configuration file
+include stdlib
+
+file_line { 'Turn off password authentation':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_cofig',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Configure private key':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
