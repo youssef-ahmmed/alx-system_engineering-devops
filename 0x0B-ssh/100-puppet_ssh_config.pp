7053685375ca18d9cf8a74f4de1ba19df3f9c6cc
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-24 17:51:10 +0300

    Edit advanced task 4: regex

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,19 @@
 # Configuration file
+
 include stdlib
 
-file_line { 'Turn off password authentication':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_cofig',
-  line    => '#    PasswordAuthentication no',
-  replace => true,
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file_line { 'Configure private key':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '#   IdentityFile ~/.ssh/school',
-  replace => true,
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
