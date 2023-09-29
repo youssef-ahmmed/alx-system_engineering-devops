1b650c94d25a664045eafac27066b4cbe937f756
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-09-29 04:02:39 +0300

    Edit advanced task 5: remove comments

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,44 +1,38 @@
-# add stable version of nginx
+# configure nginx server using puppet
+
 exec { 'add nginx stable repo':
   command => 'sudo add-apt-repository ppa:nginx/stable',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# update software packages list
 exec { 'update packages':
   command => 'apt-get update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# install nginx
 package { 'nginx':
   ensure     => 'installed',
 }
 
-# allow HTTP
 exec { 'allow HTTP':
   command => "ufw allow 'Nginx HTTP'",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
 }
 
-# change folder rights
 exec { 'chmod www folder':
   command => 'chmod -R 755 /var/www',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# create index file
 file { '/var/www/html/index.html':
   content => "Hello World!\n",
 }
 
-# create index file
 file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
 }
 
-# add redirection and error page
 file { 'Nginx default config file':
   ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
@@ -66,13 +60,12 @@ file { 'Nginx default config file':
 }
 ",
 }
-# restart nginx
+
 exec { 'restart service':
   command => 'service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-# start service nginx
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
