f2b0610e9c084890f0129fb4ce8bc262adbfe71a
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-23 09:45:57 +0300

    Add task 1

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Dictionary of list of dictionaries"""
+
+import json
+
+import requests
+
+if __name__ == '__main__':
+
+    url = "https://jsonplaceholder.typicode.com/"
+
+    users = requests.get(url + "users/").json()
+
+    dict_format = {}
+    for user in users:
+        todos = requests.get(url + "todos", params={"userId": user.get('id')}).json()
+
+        dict_format.update({
+            user.get('id'): [
+                {"username": user.get('username'),
+                 "task": task.get('title'),
+                 "completed": task.get('completed')}
+                for task in todos
+            ]
+        })
+
+    with open("todo_all_employees.json", mode='w') as f:
+        json.dump(dict_format, f)
