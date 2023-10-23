2bac5688c34db996a9f6a112272d4b2c775cf095
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-10-23 09:45:52 +0300

    Add task 1

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Export to JSON"""
+
+import json
+from sys import argv
+
+import requests
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
+
+    dict_format = {
+        user_id: [
+            {"task": task.get('title'), "completed": task.get('completed'),
+             "username": user.get('username')}
+            for task in todos
+        ]
+    }
+
+    with open("{}.json".format(user_id), mode='w') as f:
+        json.dump(dict_format, f)
