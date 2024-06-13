55390769ba62b35dcb05019d4c2b4a48e9660640
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2024-06-13 16:37:03 +0300

    Update files permissions

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,9 +3,9 @@
 exec { 'set ulimit -n 2048':
   command => "sed -i 's/15/2048/' /etc/default/nginx",
   path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-  }
+}
 
 exec { 'restart service':
   command => 'service nginx restart',
   path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
-  }
+}
